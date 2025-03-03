94d52ceba71a7cee2e6ddcd4b8e2626b3789c789
Youn Mélois <youn@melois.dev>
2025-03-03 14:19:52 +0100

    refactor: create extern decoder with decode some calls

--- src/decoder.rs
+++ src/decoder.rs
@@ -1,17 +1,19 @@
 use crate::bindings::root::oo2::*;
 use crate::error::{Error, Result};
+use crate::model::Compressor;
 use crate::util::compression::{compressor_scratch_memory_size, get_all_chunks_compressor};
 
 pub struct Decoder<'a> {
+    compressor: Compressor,
+
     compressed: &'a [u8],
-    compressed_pos: usize,
     decompressed: &'a mut [u8],
+
+    compressed_pos: usize,
     decompressed_pos: usize,
+
     dictionary_len: usize,
     check_crc: bool,
-
-    inner: OodleLZDecoder,
-    _scratch: Vec<u8>,
 }
 
 impl<'a> Decoder<'a> {
@@ -24,46 +26,14 @@ impl<'a> Decoder<'a> {
     ) -> Result<Self> {
         let compressor = get_all_chunks_compressor(&compressed, decompressed.len())?;
 
-        let decoder_size = size_of::<OodleLZDecoder>() + size_of::<u64>();
-        let scratch_size = compressor_scratch_memory_size(compressor, decompressed.len());
-
-        let memory_size = decoder_size + scratch_size;
-
-        let mut scratch = vec![0u8; scratch_size];
-
-        let decoder = OodleLZDecoder {
-            decPos: decode_start_offset as i64,
-            decLen: decompressed.len() as i64,
-            gotHeaderPos: -1,
-            resetPos: 0,
-            check: 0,
-            callsWithoutProgress: 0,
-            ownsmem: true as i32,
-            header: LZBlockHeader {
-                version: 0,
-                decodeType: 0,
-                offsetShift: 0,
-                chunkIsMemcpy: 0,
-                chunkIsReset: 0,
-                chunkHasQuantumCRCs: 0,
-            },
-            decoderSize: decoder_size as i32,
-            memorySize: memory_size as i32,
-            scratch: scratch.as_mut_ptr() as *mut _,
-            scratch_size: scratch_size as isize,
-            legacy: [0u8; 64],
-        };
-
         Ok(Self {
+            compressor,
             compressed,
-            compressed_pos: 0,
             decompressed,
+            compressed_pos: 0,
             decompressed_pos: decode_start_offset,
             dictionary_len,
             check_crc,
-
-            inner: decoder,
-            _scratch: scratch,
         })
     }
 
@@ -94,9 +64,37 @@ impl<'a> Decoder<'a> {
             curQuantumCompLen: 0,
         };
 
+        let scratch_size = compressor_scratch_memory_size(self.compressor, self.decompressed.len());
+        let mut scratch = vec![0u8; scratch_size];
+
+        let decoder_size = size_of::<OodleLZDecoder>() + size_of::<u64>();
+
+        let mut decoder = OodleLZDecoder {
+            decPos: self.decompressed_pos as i64,
+            decLen: self.decompressed.len() as i64,
+            gotHeaderPos: -1,
+            resetPos: 0,
+            check: 0,
+            callsWithoutProgress: 0,
+            ownsmem: true as i32,
+            header: LZBlockHeader {
+                version: 0,
+                decodeType: 0,
+                offsetShift: 0,
+                chunkIsMemcpy: 0,
+                chunkIsReset: 0,
+                chunkHasQuantumCRCs: 0,
+            },
+            decoderSize: decoder_size as i32,
+            memorySize: (decoder_size + scratch_size) as i32,
+            scratch: scratch.as_mut_ptr() as *mut _,
+            scratch_size: scratch_size as isize,
+            legacy: [0u8; 64],
+        };
+
         let result = unsafe {
             OodleLZDecoder_DecodeSome(
-                std::ptr::addr_of_mut!(self.inner) as *mut _,
+                std::ptr::addr_of_mut!(decoder) as *mut _,
                 &mut out,
                 self.decompressed.as_mut_ptr() as *mut _,
                 self.decompressed_pos as isize,
