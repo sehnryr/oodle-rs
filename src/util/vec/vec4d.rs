455ba7366dcc6ceb4bcb2d328d455fa60e18dc60
Youn Mélois <youn@melois.dev>
2025-03-08 16:37:57 +0100

    chore: impl vec4d methods for other types with macros

--- src/util/vec/vec4d.rs
+++ src/util/vec/vec4d.rs
@@ -40,53 +40,6 @@ where
     }
 }
 
-impl Vec4D<u32> {
-    pub fn rotate_left(mut self, shift: u32) -> Self {
-        #[cfg(feature = "simd")]
-        {
-            self.data = (self.data << shift) | (self.data >> (32 - shift));
-        }
-        #[cfg(not(feature = "simd"))]
-        {
-            self.data[0] = self.data[0].rotate_left(shift);
-            self.data[1] = self.data[1].rotate_left(shift);
-            self.data[2] = self.data[2].rotate_left(shift);
-            self.data[3] = self.data[3].rotate_left(shift);
-        }
-        self
-    }
-
-    pub fn wrapping_add(mut self, rhs: Self) -> Self {
-        #[cfg(feature = "simd")]
-        {
-            self.data += rhs.data;
-        }
-        #[cfg(not(feature = "simd"))]
-        {
-            self.data[0] = self.data[0].wrapping_add(rhs.data[0]);
-            self.data[1] = self.data[1].wrapping_add(rhs.data[1]);
-            self.data[2] = self.data[2].wrapping_add(rhs.data[2]);
-            self.data[3] = self.data[3].wrapping_add(rhs.data[3]);
-        }
-        self
-    }
-
-    pub fn wrapping_sub(mut self, rhs: Self) -> Self {
-        #[cfg(feature = "simd")]
-        {
-            self.data -= rhs.data;
-        }
-        #[cfg(not(feature = "simd"))]
-        {
-            self.data[0] = self.data[0].wrapping_sub(rhs.data[0]);
-            self.data[1] = self.data[1].wrapping_sub(rhs.data[1]);
-            self.data[2] = self.data[2].wrapping_sub(rhs.data[2]);
-            self.data[3] = self.data[3].wrapping_sub(rhs.data[3]);
-        }
-        self
-    }
-}
-
 impl<T> From<Vec4D<T>> for [T; 4]
 where
     T: Element,
@@ -103,18 +56,101 @@ where
     }
 }
 
-impl std::ops::BitXorAssign for Vec4D<u32> {
-    fn bitxor_assign(&mut self, rhs: Self) {
-        #[cfg(feature = "simd")]
-        {
-            self.data ^= rhs.data;
-        }
-        #[cfg(not(feature = "simd"))]
-        {
-            self.data[0] ^= rhs.data[0];
-            self.data[1] ^= rhs.data[1];
-            self.data[2] ^= rhs.data[2];
-            self.data[3] ^= rhs.data[3];
-        }
-    }
+macro_rules! impl_rotate_left {
+    ($($t:ty),+) => {
+        $(
+            impl Vec4D<$t> {
+                pub fn rotate_left(mut self, shift: $t) -> Self {
+                    #[cfg(feature = "simd")]
+                    {
+                        let hi = self.data >> (size_of::<$t>() as $t * 8 - shift);
+                        let lo = self.data << shift;
+
+                        self.data = lo | hi;
+                    }
+                    #[cfg(not(feature = "simd"))]
+                    {
+                        self.data[0] = self.data[0].rotate_left(shift);
+                        self.data[1] = self.data[1].rotate_left(shift);
+                        self.data[2] = self.data[2].rotate_left(shift);
+                        self.data[3] = self.data[3].rotate_left(shift);
+                    }
+                    self
+                }
+            }
+        )+
+    };
 }
+
+macro_rules! impl_wrapping_add {
+    ($($t:ty),+) => {
+        $(
+            impl Vec4D<$t> {
+                pub fn wrapping_add(mut self, rhs: Self) -> Self {
+                    #[cfg(feature = "simd")]
+                    {
+                        self.data += rhs.data;
+                    }
+                    #[cfg(not(feature = "simd"))]
+                    {
+                        self.data[0] = self.data[0].wrapping_add(rhs.data[0]);
+                        self.data[1] = self.data[1].wrapping_add(rhs.data[1]);
+                        self.data[2] = self.data[2].wrapping_add(rhs.data[2]);
+                        self.data[3] = self.data[3].wrapping_add(rhs.data[3]);
+                    }
+                    self
+                }
+            }
+        )+
+    };
+}
+
+macro_rules! impl_wrapping_sub {
+    ($($t:ty),+) => {
+        $(
+            impl Vec4D<$t> {
+                pub fn wrapping_sub(mut self, rhs: Self) -> Self {
+                    #[cfg(feature = "simd")]
+                    {
+                        self.data -= rhs.data;
+                    }
+                    #[cfg(not(feature = "simd"))]
+                    {
+                        self.data[0] = self.data[0].wrapping_sub(rhs.data[0]);
+                        self.data[1] = self.data[1].wrapping_sub(rhs.data[1]);
+                        self.data[2] = self.data[2].wrapping_sub(rhs.data[2]);
+                        self.data[3] = self.data[3].wrapping_sub(rhs.data[3]);
+                    }
+                    self
+                }
+            }
+        )+
+    };
+}
+
+macro_rules! impl_ops {
+    ($($t:ty),+) => {
+        $(
+            impl ::std::ops::BitXorAssign for Vec4D<$t> {
+                fn bitxor_assign(&mut self, rhs: Self) {
+                    #[cfg(feature = "simd")]
+                    {
+                        self.data ^= rhs.data;
+                    }
+                    #[cfg(not(feature = "simd"))]
+                    {
+                        self.data[0] ^= rhs.data[0];
+                        self.data[1] ^= rhs.data[1];
+                        self.data[2] ^= rhs.data[2];
+                        self.data[3] ^= rhs.data[3];
+                    }
+                }
+            }
+        )+
+    };
+}
+
+impl_rotate_left!(u8, u16, u32, u64, usize, i8, i16, i32, i64, isize);
+impl_wrapping_add!(u8, u16, u32, u64, usize, i8, i16, i32, i64, isize);
+impl_wrapping_sub!(u8, u16, u32, u64, usize, i8, i16, i32, i64, isize);
+impl_ops!(u8, u16, u32, u64, usize, i8, i16, i32, i64, isize);
